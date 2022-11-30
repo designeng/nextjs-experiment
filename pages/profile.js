e7ccdafe097ade854415dcbfeaad1c510eeda39c
Denis Savenok <denissavenok@gmail.com>
2022-11-30 20:19:34 +0300

    Commit for test

--- pages/profile.js
+++ pages/profile.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React from 'react';
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 // @material-ui/icons
-import Camera from "@material-ui/icons/Camera";
-import Palette from "@material-ui/icons/Palette";
-import Favorite from "@material-ui/icons/Favorite";
+import Camera from '@material-ui/icons/Camera';
+import Palette from '@material-ui/icons/Palette';
+import Favorite from '@material-ui/icons/Favorite';
 // core components
-import Header from "/components/Header/Header.js";
-import Footer from "/components/Footer/Footer.js";
-import Button from "/components/CustomButtons/Button.js";
-import GridContainer from "/components/Grid/GridContainer.js";
-import GridItem from "/components/Grid/GridItem.js";
-import HeaderLinks from "/components/Header/HeaderLinks.js";
-import NavPills from "/components/NavPills/NavPills.js";
-import Parallax from "/components/Parallax/Parallax.js";
+import Header from '/components/Header/Header.js';
+import Footer from '/components/Footer/Footer.js';
+import Button from '/components/CustomButtons/Button.js';
+import GridContainer from '/components/Grid/GridContainer.js';
+import GridItem from '/components/Grid/GridItem.js';
+import HeaderLinks from '/components/Header/HeaderLinks.js';
+import NavPills from '/components/NavPills/NavPills.js';
+import Parallax from '/components/Parallax/Parallax.js';
 
-import styles from "/styles/jss/nextjs-material-kit/pages/profilePage.js";
+import styles from '/styles/jss/nextjs-material-kit/pages/profilePage.js';
 
 const useStyles = makeStyles(styles);
 
@@ -39,7 +39,7 @@ export default function ProfilePage(props) {
         fixed
         changeColorOnScroll={{
           height: 200,
-          color: "white"
+          color: 'white',
         }}
         {...rest}
       />
@@ -52,22 +52,22 @@ export default function ProfilePage(props) {
                 <div className={classes.profile}>
                   <div>
                     <img
-                      src="/img/faces/christian.jpg"
+                      src="/img/faces/kendall.jpg"
                       alt="..."
                       className={imageClasses}
                     />
                   </div>
                   <div className={classes.name}>
-                    <h3 className={classes.title}>Christian Louboutin</h3>
+                    <h3 className={classes.title}>Lizette Brunhild</h3>
                     <h6>DESIGNER</h6>
                     <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-twitter"} />
+                      <i className={'fab fa-twitter'} />
                     </Button>
                     <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-instagram"} />
+                      <i className={'fab fa-instagram'} />
                     </Button>
                     <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-facebook"} />
+                      <i className={'fab fa-facebook'} />
                     </Button>
                   </div>
                 </div>
@@ -78,7 +78,7 @@ export default function ProfilePage(props) {
                 An artist of considerable range, Chet Faker — the name taken by
                 Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
                 and records all of his own music, giving it a warm, intimate
-                feel with a solid groove structure.{" "}
+                feel with a solid groove structure.{' '}
               </p>
             </div>
             <GridContainer justify="center">
@@ -88,7 +88,7 @@ export default function ProfilePage(props) {
                   color="primary"
                   tabs={[
                     {
-                      tabButton: "Studio",
+                      tabButton: 'Studio',
                       tabIcon: Camera,
                       tabContent: (
                         <GridContainer justify="center">
@@ -117,10 +117,10 @@ export default function ProfilePage(props) {
                             />
                           </GridItem>
                         </GridContainer>
-                      )
+                      ),
                     },
                     {
-                      tabButton: "Work",
+                      tabButton: 'Work',
                       tabIcon: Palette,
                       tabContent: (
                         <GridContainer justify="center">
@@ -154,10 +154,10 @@ export default function ProfilePage(props) {
                             />
                           </GridItem>
                         </GridContainer>
-                      )
+                      ),
                     },
                     {
-                      tabButton: "Favorite",
+                      tabButton: 'Favorite',
                       tabIcon: Favorite,
                       tabContent: (
                         <GridContainer justify="center">
@@ -191,8 +191,8 @@ export default function ProfilePage(props) {
                             />
                           </GridItem>
                         </GridContainer>
-                      )
-                    }
+                      ),
+                    },
                   ]}
                 />
               </GridItem>

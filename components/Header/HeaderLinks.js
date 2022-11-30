a5e8d44c4b3ebfce6803774d5fc515bbe4d2814a
Denis Savenok <denissavenok@gmail.com>
2022-11-30 20:30:35 +0300

    Commit for test: social link

--- components/Header/HeaderLinks.js
+++ components/Header/HeaderLinks.js
@@ -1,24 +1,24 @@
 /*eslint-disable*/
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Tooltip from "@material-ui/core/Tooltip";
-import Icon from "@material-ui/core/Icon";
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Tooltip from '@material-ui/core/Tooltip';
+import Icon from '@material-ui/core/Icon';
 
 // @material-ui/icons
-import { Apps, CloudDownload } from "@material-ui/icons";
-import DeleteIcon from "@material-ui/icons/Delete";
-import IconButton from "@material-ui/core/IconButton";
+import { Apps, CloudDownload } from '@material-ui/icons';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
 
 // core components
-import CustomDropdown from "/components/CustomDropdown/CustomDropdown.js";
-import Button from "/components/CustomButtons/Button.js";
+import CustomDropdown from '/components/CustomDropdown/CustomDropdown.js';
+import Button from '/components/CustomButtons/Button.js';
 
-import styles from "/styles/jss/nextjs-material-kit/components/headerLinksStyle.js";
+import styles from '/styles/jss/nextjs-material-kit/components/headerLinksStyle.js';
 
 const useStyles = makeStyles(styles);
 
@@ -33,7 +33,7 @@ export default function HeaderLinks(props) {
           buttonText="Components"
           buttonProps={{
             className: classes.navLink,
-            color: "transparent"
+            color: 'transparent',
           }}
           buttonIcon={Apps}
           dropdownList={[
@@ -46,7 +46,7 @@ export default function HeaderLinks(props) {
               className={classes.dropdownLink}
             >
               Documentation
-            </a>
+            </a>,
           ]}
         />
       </ListItem>
@@ -78,8 +78,8 @@ export default function HeaderLinks(props) {
         </Tooltip>*/}
         <Tooltip
           id="instagram-twitter"
-          title="Follow us on twitter"
-          placement={"top"}
+          title="Follow me on twitter"
+          placement={'top'}
           classes={{ tooltip: classes.tooltip }}
         >
           <Button
@@ -88,32 +88,32 @@ export default function HeaderLinks(props) {
             color="transparent"
             className={classes.navLink}
           >
-            <i className={classes.socialIcons + " fab fa-twitter"} />
+            <i className={classes.socialIcons + ' fab fa-twitter'} />
           </Button>
         </Tooltip>
       </ListItem>
       <ListItem className={classes.listItem}>
         <Tooltip
           id="instagram-facebook"
-          title="Follow us on facebook"
-          placement={"top"}
+          title="Follow me on facebook"
+          placement={'top'}
           classes={{ tooltip: classes.tooltip }}
         >
           <Button
             color="transparent"
-            href="https://www.facebook.com/CreativeTim?ref=creativetim"
+            href="https://www.facebook.com/profile.php?id=100006742628077"
             target="_blank"
             className={classes.navLink}
           >
-            <i className={classes.socialIcons + " fab fa-facebook"} />
+            <i className={classes.socialIcons + ' fab fa-facebook'} />
           </Button>
         </Tooltip>
       </ListItem>
       <ListItem className={classes.listItem}>
         <Tooltip
           id="instagram-tooltip"
-          title="Follow us on instagram"
-          placement={"top"}
+          title="Follow me on instagram"
+          placement={'top'}
           classes={{ tooltip: classes.tooltip }}
         >
           <Button
@@ -122,7 +122,7 @@ export default function HeaderLinks(props) {
             target="_blank"
             className={classes.navLink}
           >
-            <i className={classes.socialIcons + " fab fa-instagram"} />
+            <i className={classes.socialIcons + ' fab fa-instagram'} />
           </Button>
         </Tooltip>
       </ListItem>
